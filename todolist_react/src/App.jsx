1c079618c9136cb13283b18e9583f4f38b217e1b
Fan <3197908785@qq.com>
2024-06-22 01:57:46 +0800

    deleted:    todolist_react/node_modules/package.json 	modified:   todolist_react/src/App.css 	modified:   todolist_react/src/App.jsx 	deleted:    todolist_react/src/components/Todolist.jsx

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,70 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Button, Input, DatePicker, message } from "antd";
-import moment from "moment";
-import TodoList from "./components/Todolist";
-
-const ERROR_MESSAGE = "请输入任务名称和截止时间";
+import TodoList from "./components/List";
+import Add from "./components/Add";
 
 function App() {
-  const [messageApi, contextHolder] = message.useMessage();
-
+  //改名
   useEffect(() => {
     document.title = "Todo List";
   }, []);
 
-  const [inputValue, setInputValue] = useState("");
-  const handleOnChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const [selectedDate, setSelectedDate] = useState(null);
-  const handleDateChange = (date, dateString) => {
-    setSelectedDate(dateString);
-  };
-
   const [listData, setListData] = useState([]);
-  const handleOnClick = () => {
-    if (inputValue && selectedDate) {
-      const newItem = {
-        id: moment().valueOf(),
-        task: inputValue,
-        date: selectedDate,
-      };
-      setListData([...listData, newItem]);
-      setInputValue("");
-      setSelectedDate(null);
-    } else {
-      messageApi.open({
-        type: "error",
-        content: ERROR_MESSAGE,
-      });
-    }
-  };
 
   return (
     <div className="App">
-      {contextHolder}
+      {/* 标题 */}
       <div className="head">Todo List</div>
-      <div className="input">
-        <Input
-          onChange={handleOnChange}
-          value={inputValue}
-          placeholder="输入任务名称"
-          id="inputNote"
-        />
+
+      {/* 输入代办名与选择日期 */}
+      <div className="add">
+        <Add listData={listData} setListData={setListData}></Add>
       </div>
-      <div className="date">
-        <DatePicker
-          onChange={handleDateChange}
-          placeholder="选择截止时间"
-          id="inputDate"
-          value={selectedDate ? moment(selectedDate, "YYYY-MM-DD") : null}
-        />
-        <Button type="primary" id="add" onClick={handleOnClick}>
-          添加
-        </Button>
-      </div>
+
+      {/* 代办列表 */}
       <div className="list">
         <TodoList listData={listData} setListData={setListData} />
       </div>
